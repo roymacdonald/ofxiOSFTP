a930b384d5c4d1a8243ff7e230f00d8f36382087
Roy Macdonald <macdonald.roy@gmail.com>
2012-08-15 02:17:03 -0400

    fixed memory allocation bug while initing

--- src/ofxiOSFTP.mm
+++ src/ofxiOSFTP.mm
@@ -18,7 +18,7 @@ ofxiOSFTP::~ofxiOSFTP(){
 }
 
 void ofxiOSFTP::upload(string localFile, string remotePath){
- //   [whiteRaccoon upload: [NSString stringWithCString:localFile.c_str()] remotePath:[NSString stringWithCString:remotePath.c_str()]];
+    //   [whiteRaccoon upload: [NSString stringWithCString:localFile.c_str()] remotePath:[NSString stringWithCString:remotePath.c_str()]];
 }
 void ofxiOSFTP::list(){
     [whiteRaccoon listDirectoryContents];
@@ -34,22 +34,27 @@ string ofxiOSFTP::getHostName(){
 }
 
 void ofxiOSFTP::uploadUIImage(UIImage * img, string remotePath){
-[whiteRaccoon uploadUIImage:img remotePath:[NSString stringWithCString:remotePath.c_str()]];
-
+    [whiteRaccoon uploadUIImage:img remotePath:[NSString stringWithCString:remotePath.c_str()]];
+    
 }
 
 
-//*/
-//*
+//---------------------------------------------------------------------------
+//          obj-C implementation
+//---------------------------------------------------------------------------
 @implementation ofxiOSFTPDelegate 
-     
+
 -(id) init: (NSString *) h userName:(NSString *) u password:(NSString *)p{
     
     if(self = [super init]){			
         
+        
         self->host = [NSString stringWithString:h];
         self->user = [NSString stringWithString:u];
         self->pass = [NSString stringWithString:p];
+        [self->host retain];
+        [self->user retain];
+        [self->pass retain];
         NSLog(@" ftp init: %@ %@ %@", self->host, self->user, self->pass); 
 	}
 	return self;
@@ -79,73 +84,74 @@ void ofxiOSFTP::uploadUIImage(UIImage * img, string remotePath){
 	ofNotifyEvent(ofxiOSFTPEventDispatcher, event);
 	
     NSLog(@"%@ completed!", request);
-  /*  
-    //we cast the request to list request
-    WRRequestListDirectory * listDir = (WRRequestListDirectory *)request;
-    
-    //we print each of the files name
-    for (NSDictionary * file in listDir.filesInfo) {
-        NSLog(@"%@", [file objectForKey:(id)kCFFTPResourceName]);            
-    }
-    //*/
+    /*  
+     //we cast the request to list request
+     WRRequestListDirectory * listDir = (WRRequestListDirectory *)request;
+     
+     //we print each of the files name
+     for (NSDictionary * file in listDir.filesInfo) {
+     NSLog(@"%@", [file objectForKey:(id)kCFFTPResourceName]);            
+     }
+     //*/
 }
 -(void) uploadUIImage: (UIImage * )img remotePath:(NSString * )path{
-   // UIImage * ourImage = [UIImage imageWithCIImage:img];
+    // UIImage * ourImage = [UIImage imageWithCIImage:img];
     NSData * ourImageData = UIImageJPEGRepresentation(img, 100);
     NSLog(@" %f %f", img.size.width, img.size.height);
     [self upload:ourImageData remotePath:path];   
 }
-     
-     - (void) upload: (NSData *)data remotePath:(NSString *)remote {
-         
-         WRRequestUpload * upload = [[WRRequestUpload alloc] init];
-         upload.delegate = self;
-         
-         //for anonymous login just leave the username and password nil
-        // NSLog(@" ftp upload: %@ %@ %@", host, user, pass);          
-          // NSLog(@" ftp upload: %@ %@ %@", self->host, self->user, self->pass); 
-         /*
-         upload.hostname = self->host;
-         upload.username = self->user;
-         upload.password = self->pass;
-        //*/
-         upload.hostname = @"Roy-Macdonalds-MacBook-Pro.local";
-         upload.username = @"roy";
-         upload.password = @"panopticon";
 
-         //we set our data
-         
-         upload.sentData = data;
-         
-         upload.path =  remote;
-         cout   << [ upload.path UTF8String] << endl;
-         // @"/space.jpg";
-         
-         //we start the request
-         [upload start];
-        
-     }
+- (void) upload: (NSData *)data remotePath:(NSString *)remote {
+    
+    WRRequestUpload * upload = [[WRRequestUpload alloc] init];
+    upload.delegate = self;
+    
+    //for anonymous login just leave the username and password nil
+    // NSLog(@" ftp upload: %@ %@ %@", host, user, pass);          
+    // NSLog(@" ftp upload: %@ %@ %@", self->host, self->user, self->pass); 
+    
+     upload.hostname = self->host;
+     upload.username = self->user;
+     upload.password = self->pass;
+     
+     /*
+    upload.hostname = @"Roy-Macdonalds-MacBook-Pro.local";
+    upload.username = @"roy";
+    upload.password = @"panopticon";
+    //*/
+    //we set our data
+    
+    upload.sentData = data;
+    
+    upload.path =  remote;
+    cout   << [ upload.path UTF8String] << endl;
+    // @"/space.jpg";
+    
+    //we start the request
+    [upload start];
+    
+}
 
-     
-     -(void) requestFailed:(WRRequest *) request{
-         
-         //called after 'request' ends in error
-         //we can print the error message
-         NSLog(@"Request failed! %@", request.error.message);
-         
-         ofxiOSFTPEvent event(request.type);
-         event.bRequestFailed = true;
-         ofNotifyEvent(ofxiOSFTPEventDispatcher, event);
-         
-     }
-     
-     -(BOOL) shouldOverwriteFileWithRequest:(WRRequest *)request {
-         
-         //if the file (ftp://xxx.xxx.xxx.xxx/space.jpg) is already on the FTP server,the delegate is asked if the file should be overwritten 
-         //'request' is the request that intended to create the file
-         return YES;
-         
-     }
-     
+
+-(void) requestFailed:(WRRequest *) request{
+    
+    //called after 'request' ends in error
+    //we can print the error message
+    NSLog(@"Request failed! %@", request.error.message);
+    
+    ofxiOSFTPEvent event(request.type);
+    event.bRequestFailed = true;
+    ofNotifyEvent(ofxiOSFTPEventDispatcher, event);
+    
+}
+
+-(BOOL) shouldOverwriteFileWithRequest:(WRRequest *)request {
+    
+    //if the file (ftp://xxx.xxx.xxx.xxx/space.jpg) is already on the FTP server,the delegate is asked if the file should be overwritten 
+    //'request' is the request that intended to create the file
+    return YES;
+    
+}
+
 @end
 //*/
